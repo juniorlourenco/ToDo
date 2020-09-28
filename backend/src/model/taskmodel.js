1e71b155cfdeef6672e689b9cb095a5ae033568b
Junior Lourenço <juniorminaj101@gmail.com>
2020-09-28 08:13:31 -0400

    task schema

--- backend/src/model/taskmodel.js
+++ backend/src/model/taskmodel.js
@@ -8,4 +8,8 @@ const TaskSchema = new Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
     when: {type: Date, required: true},
+    done: {type: Boolean, default: false},
+    created: {type: Date, default: Date.now()}
 });
+
+module.exports = mongoose.model('Task', TaskSchema);
