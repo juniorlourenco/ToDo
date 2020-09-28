34418bcbb2926de6a0125176793fbfad0bbc9843
Junior Lourenço <juniorminaj101@gmail.com>
2020-09-28 13:47:54 -0400

    validate macaddress and duplicated tasks

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ const server = express();
 server.use(express.json());
 
 const TaskRoutes = require('./routes/TaskRoutes');
-server. use('/task', TaskRoutes);
-server.listen(3000, () => {
+server.use('/task', TaskRoutes);
+server.listen(3335, () => {
     console.log('Api online');
 });
--- backend/src/model/TaskModel.js
+++ backend/src/model/TaskModel.js
@@ -3,7 +3,7 @@ const mongoose = require('../config/database');
 const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
-    macadress: {type: String, required: true},
+    macaddress: {type: String, required: true},
     type: {type: Number, required: true},
     title: {type: String, required: true},
     description: {type: String, required: true},
