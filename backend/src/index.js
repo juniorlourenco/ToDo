be32b0d8a222ba3e251890f645dbd65cb087fc61
Junior Lourenço <juniorminaj101@gmail.com>
2020-09-28 08:25:22 -0400

    adding task route

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,8 @@ const express = require('express');
 const server = express();
 server.use(express.json());
 
-server.get('/teste', (req, res) => {
-    res.send('Tudo certo com a nossa API!');
-});
-
+const TaskRoutes = require('./routes/TaskRoutes');
+server. use('/task', TaskRoutes);
 server.listen(3000, () => {
     console.log('Api online');
 });
