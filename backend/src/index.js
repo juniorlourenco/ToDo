8978ac17c6ee96c02d471f3d940b8e019f754c1d
Junior Lourenço <juniorminaj101@gmail.com>
2020-09-28 14:03:10 -0400

    listing all tasks

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,6 @@ server.use(express.json());
 
 const TaskRoutes = require('./routes/TaskRoutes');
 server.use('/task', TaskRoutes);
-server.listen(3335, () => {
+server.listen(3000, () => {
     console.log('Api online');
 });
