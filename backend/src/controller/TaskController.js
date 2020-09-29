19babe10d3f8e3f334621708fce2913e2d43e8fb
Junior Lourenço <juniorminaj101@gmail.com>
2020-09-29 13:38:59 -0400

    listing today tasks

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -1,4 +1,5 @@
 const TaskModel = require('../model/TaskModel');
+const {startOfDay, endOfDay} = require('date-fns');
 
 const current = new Date();
 class TaskController{
@@ -72,8 +73,7 @@ class TaskController{
     }
 
     async late(req, res) {
-        await TaskModel
-        .find({
+        await TaskModel.find({
             'when': {'$lt': current},
             'macaddress': {'$in': req.body.macaddress}
         })
@@ -84,8 +84,22 @@ class TaskController{
         .catch(error => {
             return res.status(500).json(error);
         })
-
     }
-}
+
+    async today(req, res) {
+        await TaskModel
+            .find({
+                'macaddress': {'$in': req.body.macaddress},
+                'when': {'$gte': startOfDay(current), '$lte': endOfDay(current)}
+            })
+            .sort('when')
+            .then(response => {
+                return res.status(200).json(response);
+            })
+            .catch(error => {
+                return res.status(500).json(error);
+            });
+    }
+} 
 
 module.exports = new TaskController();
