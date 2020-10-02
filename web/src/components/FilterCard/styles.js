ab3798f25caf703e1dd08d5c7e61831467a77fc1
Junior Lourenço <juniorminaj101@gmail.com>
2020-10-02 14:02:35 -0400

    creating filter component with styled components

---
+++ web/src/components/FilterCard/styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+width: 80%;
+height: 60 px;
+background: ${props => props.actived ? '#EE6B26' : '#20295F'};
+padding: 10px;
+cursor: pointer;
+
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+border-radius: 5px;
+
+img {
+    width: 25px;
+    height: 25px;
+}
+
+span {
+    color:#FFF;
+    font-weight: bold;
+    align-self: flex-end;
+    font-size: 18px;
+}
+
+&:hover{
+    background: #EE6B26;
+}
+
+
+`
--- web/src/views/Home/styles.js
+++ web/src/views/Home/styles.js
@@ -1,6 +1,21 @@
 import styled from 'styled-components';
 
 export const Container = styled.div `
+width: 100%;
+`
+
+export const FilterArea = styled.div `
+display: flex;
+width: 100%;
+justify-content: space-around;
+margin-top: 30px;
+
+button {
+    background: none;
+    border: none;
     width: 100%;
-    background-color: red;
+    justify-content: space-around;
+    margin-top: 30px;
+    outline: none; //resetando config google chrome 
+}
 `
